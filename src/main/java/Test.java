0a26aac1528c55933e9e999ca6d0bde7ff91adbc
zhoufengchao <1991338921@qq.com>
2018-07-11 21:27:26 +0800

    第三次提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,4 +2,13 @@
  * Created by 哥只是个传说 on 2018/7/11 0011.
  */
 public class Test {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
