9e94f7ede32d3a6af233bbaf0d66145b036a670d
zhoufengchao <1991338921@qq.com>
2018-07-11 21:33:58 +0800

    第三次提交

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,16 @@
  * Created by 哥只是个传说 on 2018/7/11 0011.
  */
 public class Test {
+    private String password;
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     private String name;
 
     public String getName() {
